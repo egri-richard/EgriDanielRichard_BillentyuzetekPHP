b6f5b3840af35ac8ff7da1242b9c6848deb45f6f
Erinad-2002 <Erinad.business@gmail.com>
2021-10-30 17:04:35 +0200

    keyboard class + __construct()

---
+++ Keyboard.php
@@ -0,0 +1,18 @@
+<?php
+
+class Keyboard {
+    private $id;
+    private $listahoz_adva;
+    private $nev;
+    private $ar;
+    private $mechanikus;
+    private $hattervilagitas;
+
+    public function __construct(DateTime $listahoz_adva,
+                                String $nev,
+                                Int $ar,
+                                Boolean $mechanikus
+                                Boolean $hattervilagitas)
+}
+
+?>
