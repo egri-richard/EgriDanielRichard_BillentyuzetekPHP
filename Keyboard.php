1b0011782536f6af3f40729df29db06714ad38d4
Erinad-2002 <Erinad.business@gmail.com>
2021-10-31 11:43:45 +0100

    commit

--- Keyboard.php
+++ Keyboard.php
@@ -11,8 +11,27 @@ class Keyboard {
     public function __construct(DateTime $listahoz_adva,
                                 String $nev,
                                 Int $ar,
-                                Boolean $mechanikus
-                                Boolean $hattervilagitas)
+                                Boolean $mechanikus,
+                                Boolean $hattervilagitas) {
+                                    
+        $this->listahoz_adva = $listahoz_adva;
+        $this->nev = $nev;
+        $this->ar = $ar;
+        $this->mechanikus = $mechanikus;
+        $this->hattervilagitas = $hattervilagitas;
+    }
+
+    public function getId() : int {
+        return $this->id;
+    }
+
+    public function getListahoz_adva() : DateTime {
+        return $this->listahoz_adva;
+    }
+
+    public function getNev() : String {
+        return $this->nev;
+    }
 }
 
 ?>
